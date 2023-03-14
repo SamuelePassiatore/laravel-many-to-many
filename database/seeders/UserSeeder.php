6ea616d79bbcb5ae0cf7903f2437e34bc8c1f995
SamuelePassiatore <semmisno2@protonmail.com>
2023-03-14 18:36:38 +0100

    added all columnof table user detail

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,7 +22,7 @@ class UserSeeder extends Seeder
 
         $user->save();
 
-        for ($i = 0; $i < 3; $i++) {
+        for ($i = 0; $i < 2; $i++) {
 
             $user = new User();
             $user->name = $faker->firstName();
